e28f8f334e57dce9408a6e3f9f4723ead04f401e
Felix Knorre <felix-knorre@hotmail.de>
2019-04-13 15:32:49 +0200

    updated the index

--- src/DBFeldFixTest.java
+++ src/DBFeldFixTest.java
@@ -1,29 +1,35 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+
 import org.junit.jupiter.api.Test;
 
+
+
+
 class DBFeldFixTest {
+	
+
 	
 
 	@Test
 	void testRemoveLast() {
 		//create new db
-		DBFeldFix dbFeldFix = new DBFeldFix(10);
+		DBFeldFix<Igel> dbFeldFix = new DBFeldFix<Igel>(10);
 		//create new igel
-		Igel igel0 = new Igel(1, 2, 3);
-		Igel igel1 = new Igel(4, 5, 7);
-		Igel igel2 = new Igel(8, 9, 10);
-		Igel igel3 = new Igel(11, 12, 13);
+		Igel igel0 = new Igel(1, 2);
+		Igel igel1 = new Igel(4, 5);
+		Igel igel2 = new Igel(8, 9);
+		Igel igel3 = new Igel(11, 12);
 		//append igels
 		dbFeldFix.appendLast(igel0);
 		dbFeldFix.appendLast(igel1);
 		dbFeldFix.appendLast(igel2);
 		dbFeldFix.appendLast(igel3);
 		//def. the expected outcome
-		int[] expected = {igel3.weight(), igel3.size(), igel3.getStacheln()};
+		int[] expected = {igel3.weight(), igel3.size()};
 		//def. actual outcome
 		Igel actualIgel = dbFeldFix.removeLast();
-		int[] actual = {actualIgel.weight(), actualIgel.size(), actualIgel.getStacheln()};
+		int[] actual = {actualIgel.weight(), actualIgel.size()};
 		//compare
 		assertArrayEquals(expected, actual, "The method should remove and return the last item");
 	}
@@ -31,21 +37,21 @@ class DBFeldFixTest {
 	@Test
 	void testGet() {
 		//create new db
-		DBFeldFix dbFeldFix = new DBFeldFix(10);
+		DBFeldFix<Igel> dbFeldFix = new DBFeldFix<Igel>(10);
 		//create new igel
-		Igel igel0 = new Igel(1, 2, 3);
-		Igel igel1 = new Igel(4, 5, 7);
-		Igel igel2 = new Igel(8, 9, 10);
-		Igel igel3 = new Igel(11, 12, 13);
+		Igel igel0 = new Igel(1, 2);
+		Igel igel1 = new Igel(4, 5);
+		Igel igel2 = new Igel(8, 9);
+		Igel igel3 = new Igel(11, 12);
 		//append igels
 		dbFeldFix.appendLast(igel0);
 		dbFeldFix.appendLast(igel1);
 		dbFeldFix.appendLast(igel2);
 		dbFeldFix.appendLast(igel3);
 		//def. the expected outcome
-		int[] expected = {igel0.weight(), igel0.size(), igel0.getStacheln()};
+		int[] expected = {igel0.weight(), igel0.size()};
 		//def. actual outcome
-		int[] actual = {dbFeldFix.get(0).weight(), dbFeldFix.get(0).size(), dbFeldFix.get(0).getStacheln()};
+		int[] actual = {dbFeldFix.get(1).weight(), dbFeldFix.get(1).size()};
 		//compare
 		assertArrayEquals(expected, actual, "The method should get the item at the position x");
 	}
@@ -53,23 +59,23 @@ class DBFeldFixTest {
 	@Test
 	void testAppendLast() {
 		//create new db
-		DBFeldFix dbFeldFix = new DBFeldFix(10);
+		DBFeldFix<Igel> dbFeldFix = new DBFeldFix<Igel>(10);
 		//create new igel
-		Igel igel0 = new Igel(1, 2, 3);
-		Igel igel1 = new Igel(4, 5, 7);
-		Igel igel2 = new Igel(8, 9, 10);
-		Igel igel3 = new Igel(11, 12, 13);
+		Igel igel0 = new Igel(1, 2);
+		Igel igel1 = new Igel(4, 5);
+		Igel igel2 = new Igel(8, 9);
+		Igel igel3 = new Igel(11, 12);
 		//append igels
 		dbFeldFix.appendLast(igel0);
 		dbFeldFix.appendLast(igel1);
 		dbFeldFix.appendLast(igel2);
 		dbFeldFix.appendLast(igel3);
 		//def. expected outcome
-		Igel[] expectedIgels = {igel0, igel1, igel2, igel3, null, null, null, null, null, null};
+		Igel[] expectedIgels = {null, igel0, igel1, igel2, igel3, null, null, null, null, null, null};
 		//compare
-		for (int i = 0; i < dbFeldFix.size; i++) {
-			int[] expected = {expectedIgels[i].weight(), expectedIgels[i].size(), expectedIgels[i].getStacheln()};
-			int[] actual = {dbFeldFix.get(i).weight(), dbFeldFix.get(i).size(), dbFeldFix.get(i).getStacheln()};
+		for (int i = 1; i < dbFeldFix.size; i++) {
+			int[] expected = {expectedIgels[i].weight(), expectedIgels[i].size()};
+			int[] actual = {dbFeldFix.get(i).weight(), dbFeldFix.get(i).size()};
 			assertArrayEquals(expected, actual, "The method should append a item to the db");
 			
 		}
@@ -78,25 +84,25 @@ class DBFeldFixTest {
 	@Test
 	void testSwap() {
 		//create new db
-		DBFeldFix dbFeldFix = new DBFeldFix(10);
+		DBFeldFix<Igel> dbFeldFix = new DBFeldFix<Igel>(10);
 		//create new igel
-		Igel igel0 = new Igel(1, 2, 3);
-		Igel igel1 = new Igel(4, 5, 7);
-		Igel igel2 = new Igel(8, 9, 10);
-		Igel igel3 = new Igel(11, 12, 13);
+		Igel igel0 = new Igel(1, 2);
+		Igel igel1 = new Igel(4, 5);
+		Igel igel2 = new Igel(8, 9);
+		Igel igel3 = new Igel(11, 12);
 		//append igels
 		dbFeldFix.appendLast(igel0);
 		dbFeldFix.appendLast(igel1);
 		dbFeldFix.appendLast(igel2);
 		dbFeldFix.appendLast(igel3);
 		//def. expected items in position x
-		int[] expectedpos1 = {igel3.weight(), igel3.size(), igel3.getStacheln()};
-		int[] expectedpos3 = {igel1.weight(), igel1.size(), igel1.getStacheln()};
+		int[] expectedpos1 = {igel2.weight(), igel2.size()};
+		int[] expectedpos3 = {igel0.weight(), igel0.size()};
 		//swap item 1 and item 3
 		dbFeldFix.swap(1, 3);
 		//get actual items in position x
-		int[] actualpos1 = {dbFeldFix.get(1).weight(), dbFeldFix.get(1).size(), dbFeldFix.get(1).getStacheln()};
-		int[] actualpos3 = {dbFeldFix.get(3).weight(), dbFeldFix.get(3).size(), dbFeldFix.get(3).getStacheln()};
+		int[] actualpos1 = {dbFeldFix.get(1).weight(), dbFeldFix.get(1).size()};
+		int[] actualpos3 = {dbFeldFix.get(3).weight(), dbFeldFix.get(3).size()};
 		//compare
 		assertArrayEquals(expectedpos1, actualpos1, "The method should swap two items in the db");
 		assertArrayEquals(expectedpos3, actualpos3, "The method should swap two items in the db");
